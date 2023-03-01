a0243f64ae3ebf4c0ade7ee8bc5ac9da56bf5b51
hidayat78 <mdhidayat78@gmail.com>
2023-03-01 17:15:57 +0530

    new project

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ChakraProvider, theme } from "@chakra-ui/react";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
+
+export const server = `https://api.coingecko.com/api/v3`;
